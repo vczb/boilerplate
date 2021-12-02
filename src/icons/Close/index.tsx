dad8ffb7a593928b06b929bc56546f60cd9fae9c
Vinicius Zucatti <viniciuszucatti@gmail.com>
2021-12-01 22:56:32 -0300

    Add Close icon

---
+++ src/icons/Close/index.tsx
@@ -0,0 +1,14 @@
+const Close = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    data-name="Layer 1"
+    viewBox="0 0 100 100"
+    x="0px"
+    y="0px"
+  >
+    <path d="M30 72a2 2 0 0 1-1.41-3.41l40-40a2 2 0 1 1 2.83 2.83l-40 40A2 2 0 0 1 30 72Z" />
+    <path d="M70 72a2 2 0 0 1-1.41-.59l-40-40a2 2 0 1 1 2.83-2.83l40 40A2 2 0 0 1 70 72Z" />
+  </svg>
+)
+
+export default Close
