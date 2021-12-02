c6d92f9370041bf6537764dc3b23b3faa73e7d0f
Vinicius Zucatti <viniciuszucatti@gmail.com>
2021-12-01 22:08:01 -0300

    Create Home template

--- src/components/Main/index.tsx
+++
@@ -1,5 +0,0 @@
-import * as S from './styles'
-
-const Main = () => <S.Wrapper></S.Wrapper>
-
-export default Main
---
+++ src/templates/Home/index.tsx
@@ -0,0 +1,10 @@
+import Header from 'components/Header'
+import * as S from './styles'
+
+const Home = () => (
+  <S.Wrapper>
+    <Header />
+  </S.Wrapper>
+)
+
+export default Home
