fae2273754da86f4afad6d485eab4bbf0c513a3a
Vinicius Zucatti <viniciuszucatti@gmail.com>
2021-12-01 22:05:39 -0300

    Fix storybook settings

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,12 +1,16 @@
-
+import { ThemeProvider } from "styled-components"
+import theme from '../src/styles/theme'
 import GlobalStyles from '../src/styles/global';
+import { PortalProvider } from '../src/hooks/use-portal'
 
 export const decorators = [
   (Story) => (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyles/>
-      <Story/>
-    </>
+      <PortalProvider>
+        <Story/>
+      </PortalProvider>
+    </ThemeProvider>
   )
 ]
 
